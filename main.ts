033ee63036f10c89caeee37710ecd0d9679d59ef
Ryan Murphy <rdmurphy@users.noreply.github.com>
2023-03-01 22:49:16 -0800

    Flesh out docs and such

--- main.ts
+++ main.ts
@@ -1,3 +1,96 @@
+// Copyright 2023 Ryan Murphy. MIT license.
+
+/**
+ * A port of the [`quaff` Node.js library](https://github.com/rdmurphy/quaff) to
+ * Deno. Mostly done as an experiment to see how easy it would be, but it's
+ * fully tested and adds support for [TOML](https://toml.io/en/)!
+ *
+ * ## Usage
+ *
+ * Assume a folder with this structure.
+ *
+ * ```txt
+ * data/
+ * mammals/
+ * cats.json
+ * dogs.json
+ * bears.csv
+ * birds/
+ * parrots.yml
+ * story.aml
+ * ```
+ *
+ * After importing `quaff`:
+ *
+ * ```js
+ * import { load } from "https://deno.land/x/quaff/main.ts";
+ *
+ * const data = await load("./data/");
+ * console.log(data);
+ * ```
+ *
+ * And the results...
+ *
+ * ```json
+ * {
+ *   "mammals": {
+ *     "cats": ["Marty", "Sammy"],
+ *     "dogs": ["Snazzy", "Cally"],
+ *     "bears": [
+ *       {
+ *         "name": "Steve",
+ *         "type": "Polar bear"
+ *       },
+ *       {
+ *         "name": "Angelica",
+ *         "type": "Sun bear"
+ *       }
+ *     ]
+ *   },
+ *   "birds": {
+ *     "parrots": {
+ *       "alive": ["Buzz"],
+ *       "dead": ["Moose"]
+ *     },
+ *     "story": {
+ *       "title": "All about birds",
+ *       "prose": [
+ *         { "type": "text", "value": "Do you know how great birds are?" },
+ *         { "type": "text", "value": "Come with me on this journey." }
+ *       ]
+ *     }
+ *   }
+ * }
+ * ```
+ *
+ * It's also possible to load a single file at a time, allowing more custom
+ * approaches in case `load` doesn't work exactly the way you'd like.
+ *
+ * ```ts
+ * import { loadFile } from "https://deno.land/x/quaff/main.ts";
+ *
+ * const data = await loadFile("./data/mammals/bears.csv");
+ * console.log(data);
+ * ```
+ *
+ * And the results...
+ *
+ * ```json
+ * [
+ *   {
+ *    "name": "Steve",
+ *    "type": "Polar bear"
+ *  },
+ *  {
+ *    "name": "Angelica",
+ *    "type": "Sun bear"
+ *  }
+ * ]
+ * ```
+ *
+ * @module
+ */
+
 import {
   dset,
   extname,
@@ -18,6 +111,12 @@ import {
  */
 const exts = [".json", ".yaml", ".yml", ".csv", ".tsv", ".toml", ".aml"];
 
+/**
+ * Loads a single file and returns the data if it's a valid format.
+ *
+ * @param filePath A path to the file to load
+ * @returns The post-processed data from the file as a JavaScript object
+ */
 export async function loadFile(filePath: string): Promise<unknown> {
   const ext = extname(filePath);
 
@@ -62,11 +161,14 @@ export async function loadFile(filePath: string): Promise<unknown> {
   return data;
 }
 
-/**
- * We know this will return a string-keyed Object, but that's about it.
- */
 export type LoadReturnValue = Record<string, unknown>;
 
+/**
+ * Loads a directory of files and returns the data as a JavaScript object.
+ *
+ * @param dirPath The path to the directory to load
+ * @returns An object with the post-processed data from the files in the directory
+ */
 export async function load(dirPath: string): Promise<LoadReturnValue> {
   // normalize the input path
   const cwd = normalize(dirPath);
